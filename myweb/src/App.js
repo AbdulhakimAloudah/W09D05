39f00e8ef11ed544213b78c315e16fb145c55aa3
AbdulhakimAloudah <Abdulhakim.Ali.elq.js@tuwaiq.edu.sa>
2021-12-11 20:05:15 +0300

    dflgkjhn

--- myweb/src/App.js
+++ myweb/src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import Home from "./components/home";
+import Login from "./components/Login";
+import SignUp from "./components/SignUp";
+import Forgot from "./components/horget";
+import Activated from "./components/activated";
+import PasswordReset from "./components/passwordReset";
+import Posts from "./components/posts";
+import Post from "./components/post";
+import { useNavigate } from "react-router-dom";
 
 function App() {
+  const navigate = useNavigate();
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route exact path="/login" element={<Login />} />
+        <Route exact path="/signup" element={<SignUp />} />
+        <Route exact path="/forgot" element={<Forgot />} />
+        <Route exact path="/activate/:token" element={<Activated />} />
+        <Route exact path="/forgot/:token" element={<PasswordReset />} />
+        <Route exact path="/post/:id" element={<Post />} />
+        <Route exact path="/posts" element={<Posts />} />
+        <Route
+          path="*"
+          element={
+            <>
+            
+              <button onClick={() => navigate("/")}>Home</button>
+            </>
+          }
+        />
+      </Routes>
     </div>
   );
 }
